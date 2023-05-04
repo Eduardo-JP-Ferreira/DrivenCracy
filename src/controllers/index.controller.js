9d906813474f2792efa05ae8c53fa96baa0c3e3e
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2023-05-04 09:02:03 -0300

    Implementação postVote

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -44,18 +44,32 @@ export async function postChoice(req, res) {
             res.status(404).send("Enquete Inexistente")
         }
         else{
-            const verifyTitle= await db.collection("choice").findOne({title})
-            if(!verifyTitle){
-                const object = {
-                    title,
-                    pollId  
-                }
-                await db.collection("choice").insertOne(object);
-                return res.status(201).send("created")
+        const data = new Date()        
+        const date = data.toISOString().slice(0, 16).replace("T", " ")
+
+            if(date>verifyPoll.expireAt){
+                // console.log("expirou")
+                res.status(403).send("Esta enquete já expirou")
             }
             else{
-                res.send(409)
+                // console.log("OK")
+                const choices= await db.collection("choice").find({pollId}).toArray()
+                const verifyTitle = choices.find( choice => choice.title === title );
+                if(!verifyTitle){
+                    const object = {
+                        title,
+                        pollId  
+                    }
+                    await db.collection("choice").insertOne(object);
+                    return res.status(201).send("created")
+                }
+                else{
+                    res.status(409).send("Este titulo já existe")
+                }
+                // res.status(200).send(choices)
+                // res.status(200).send(verifyTitle)
             }
+              
         }        
     }catch (err){
         res.status(500).send(err.message)
@@ -79,9 +93,54 @@ export async function getChoice(req, res) {
 }
 
 export async function postVote(req, res) {
-  
+    const choiceId= req.params.id; 
+    try {
+
+        const verifyChoice = await db.collection("choice").findOne({_id: new ObjectId(choiceId)})
+        if(!verifyChoice){
+            res.status(404).send("Escolha Inexistente")
+        }
+        else{
+            const data = new Date()        
+            const date = data.toISOString().slice(0, 16).replace("T", " ")
+            
+            const verifyPoll = await db.collection("poll").findOne({_id: new ObjectId(verifyChoice.pollId)})
+
+            if(date>verifyPoll.expireAt){
+
+                res.status(403).send("Esta enquete já expirou")
+            }
+            else{
+                const voteObject={
+                    choiceId,
+                    date
+                }
+                await db.collection("votes").insertOne(voteObject);
+                return res.status(201).send("created")
+            }
+        }
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+   
+
 }
+
 export async function getResult(req, res) {
-  
+    const id= req.params.id; 
+
+    try {
+        const verifyPoll = await db.collection("poll").findOne({_id: new ObjectId(id)})
+        if(!verifyPoll){
+            res.status(404).send("Enquete Inexistente")
+        }
+        else{
+
+        }
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 }
 

6bd0155073ffdbf9b3d87164199e52364303cb0e
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2023-05-04 10:01:19 -0300

    Implementação getResult

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { choiceObject, pollObject } from "../schema/validate.schema.js";
 import { getChoice, getPoll, getResult,
+    getVote,
 postChoice, postPoll, postVote } from "../controllers/index.controller.js";
 
 const router = Router()
@@ -13,6 +14,7 @@ router.post("/choice", validateSchema(choiceObject), postChoice)
 router.get("/poll/:id/choice", getChoice)
 
 router.post("/choice/:id/vote", postVote)
+router.get("/votes", getVote)
 router.get("/poll/:id/result", getResult)
 
 export default router
